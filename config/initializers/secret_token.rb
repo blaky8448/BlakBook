202796dbac7faf7d2acdfa68dddd06d44df044cc
Joviltas Strautnikas <Jstrautnikas@yahoo.com>
2013-04-15 18:18:59 +0100

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlakBook::Application.config.secret_token = '36d4159595760b67a1cd6eab4368cd66dda12705da83374f8c2cdd84419c3f4976aea32eff2c3a8c10a94c91dbbc9d26a314927b552c86afe9ecd28600d5eb43'
